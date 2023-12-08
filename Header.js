d85b021ff76554855013fa60096320c2163da5a8
Soda-art <maimai0906096321@gmail.com>
2023-12-08 16:13:49 +0700

    first commit

---
+++ Header.js
@@ -0,0 +1,17 @@
+
+export default function Header() {
+    return (
+      <header>
+        <nav className="nav">
+          <img className="nav-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png"
+          />
+          <ul className="nav-items">
+              <li>Pricing</li>
+              <li>About</li>
+              <li>Contact</li>
+              </ul>
+        </nav>
+      </header>
+    );
+  }
